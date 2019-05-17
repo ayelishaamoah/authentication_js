3e586e1a5ed97ce94ac289f922cdf4bae3e2c1aa
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-05-17 11:14:48 +0100

    Add test for incomplete fields

--- cypress/integration/features_spec.js
+++ cypress/integration/features_spec.js
@@ -1,13 +1,21 @@
+// User can fill out form
 describe('sign up', function() {
   it('a new user can sign up for makers bnb', function() {
     cy.signup('John','Doe', 'test@example', 'password123', 'password123');
     cy.contains('Feel at home, anywhere')
   });
 });
-// 
-// describe('sign up', function() {
-//   it('a new user can sign up for makers bnb', function() {
-//     cy.login('test@example', 'password123')
-//     cy.contains('Welcome John Doe')
-//   });
-// });
+
+// validate form
+describe('sign up', function() {
+  context('invalid credentials', function(){
+    it('u user must fill in all fields', function() {
+      cy.visit('/')
+      cy.get('input[name="first_name"]').type("John")
+      cy.get('input[name="email"]').type('test@example')
+      cy.get('input[name="password"]').type('password123')
+      cy.get('button[id="sign_up"]').click()
+      cy.contains('Please fill out all fields');
+    });
+  });
+});
